ea8331d6f1ac259929d707c2949b9c7bdeff17fa
marielohcs <m.loh@imperial.ac.uk>
2021-08-12 14:38:49 +0800

    Update 3_Plot_missing_data.R

--- 3_Plot_missing_data.R
+++ 3_Plot_missing_data.R
@@ -2,20 +2,10 @@ library(bcjupyterimport)
 library(ggplot2)
 library(naniar) #Load library to plot missingness in dataset
 
-#API fetch data from BCP
-res = fetchData ("select * from ds101079")
+#Fetch data from BC server and read them into a table
+res = fetchData ("select * from ds101103")
 bcos_data <- read.table(res$RESULT, sep="\t", header = TRUE)
 
-#Plot missing data in whole dataset
-vis_miss(bcos_data)
 
-#Plot missing data in specific column
+#Plot missing data for specific column
 vis_miss(bcos_data[,1:20])
-
-#Count number of column with NA value
-#n_var_miss(bcos_data)
-
-#Use UpSetR to plot number of dataset with missing data
-#gg_miss_upset(bcos_data)
-#gg_miss_upset(bcos_data[,10:ncol(bcos_data)], nsets = n_var_miss(bcos_data)) #nsets determinbce the number of sets (up tp 40)
-
