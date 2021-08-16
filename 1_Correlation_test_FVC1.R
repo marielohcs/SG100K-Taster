00b92d8f7072a3a38b17b132eb657795096ae567
marielohcs <m.loh@imperial.ac.uk>
2021-08-16 17:40:06 +0800

    Update 1_Correlation_test_FVC1.R

--- 1_Correlation_test_FVC1.R
+++ 1_Correlation_test_FVC1.R
@@ -4,7 +4,7 @@ library(ggpubr)
 library(grid)
 
 #Fetch data from BC server and read them into a table
-res = fetchData ("select * from ds101103")
+res = fetchData ("select * from ds101184")
 bcos_data <- read.table(res$RESULT, sep="\t", header = TRUE)
 attach(bcos_data)
 
@@ -21,8 +21,8 @@ cor.test(bcos_data$DBI14_WEIGHT, bcos_data$DSM7_FVC_1, method = c("pearson"))
 correlation_height <- cor.test(bcos_data$DBI13_HEIGHT, bcos_data$DSM7_FVC_1, method = c("pearson"))$estimate
 correlation_weight <- cor.test(bcos_data$DBI14_WEIGHT, bcos_data$DSM7_FVC_1, method = c("pearson"))$estimate
 
-t.test(DSM7_FVC_1 ~ FREG7_GENDER2_LABEL)
-ttest_gender <- t.test(DSM7_FVC_1 ~ FREG7_GENDER2_LABEL)
+t.test(DSM7_FVC_1 ~ FREG7_GENDER_LABEL)
+ttest_gender <- t.test(DSM7_FVC_1 ~ FREG7_GENDER_LABEL)
 
 correlation_age <- cor.test(bcos_data$FREG8_AGE, bcos_data$DSM7_FVC_1, method = c("pearson"))$estimate
 
@@ -41,13 +41,13 @@ B <- ggplot(bcos_data, mapping = aes(x = DBI14_WEIGHT , y = DSM7_FVC_1)) +
   geom_smooth(method='lm', formula = y~x, color = "black") + 
   annotate("text", x =45, y = 6, label = paste("R=",(round(correlation_weight,3))))
 
-C <- ggplot(bcos_data, mapping = aes(x = FREG8_AGE2 , y = DSM7_FVC_1)) + 
+C <- ggplot(bcos_data, mapping = aes(x = FREG8_AGE , y = DSM7_FVC_1)) + 
   geom_point() +
   labs(x = "FREG8_AGE", y = "DSM7_FVC_1", title = "FREG8_AGE & DSM7_FVC_1") +
   geom_smooth(method='lm', formula = y~x, color = "black") + 
   annotate("text", x =70, y = 6, label = paste("R=",(round(correlation_age,3))))
 
-D <- ggplot(bcos_data, mapping = aes(x = as.factor(FREG7_GENDER2_LABEL) , y = DSM7_FVC_1)) + 
+D <- ggplot(bcos_data, mapping = aes(x = as.factor(FREG7_GENDER_LABEL) , y = DSM7_FVC_1)) + 
   geom_boxplot() +
   labs(x = "FREG7_GENDER", y = "DSM7_FVC_1", title = "FREG7_GENDER & DSM7_FVC_1") +
   annotate("text", x = 1.5, y = 5, label = paste("P value =",(pvalue_gender)))
