9f6fb503fb483eae49f4c41c0db0c582eb06acb0
marielohcs <m.loh@imperial.ac.uk>
2021-08-16 17:40:45 +0800

    Update 2_Correlation_test_FEV1.R

--- 2_Correlation_test_FEV1.R
+++ 2_Correlation_test_FEV1.R
@@ -4,7 +4,7 @@ library(ggpubr)
 library(grid)
 
 #Fetch data from BC server and read them into a table
-res = fetchData ("select * from ds101080")
+res = fetchData ("select * from ds101184")
 bcos_data <- read.table(res$RESULT, sep="\t", header = TRUE)
 
 bcos_data = as.data.frame(bcos_data)
@@ -13,13 +13,10 @@ attach(bcos_data)
 correlation_height <- cor.test(bcos_data$DBI13_HEIGHT, bcos_data$DSM8_FEV1_1, method = c("pearson"))$estimate
 correlation_weight <- cor.test(bcos_data$DBI14_WEIGHT, bcos_data$DSM8_FEV1_1, method = c("pearson"))$estimate
 
-FVC_1_male <- bcos_data[bcos_data$FREG7_GENDER2_LABEL == "Male",]
-FVC_1_female <- bcos_data[bcos_data$FREG7_GENDER2_LABEL == "Female",]
+t.test(DSM8_FEV1_1 ~ FREG7_GENDER_LABEL)
+ttest_gender <- t.test(DSM8_FEV1_1 ~ FREG7_GENDER_LABEL)
 
-t.test(DSM8_FEV1_1 ~ FREG7_GENDER2_LABEL)
-ttest_gender <- t.test(DSM8_FEV1_1 ~ FREG7_GENDER2_LABEL)
-
-correlation_age <- cor.test(bcos_data$FREG8_AGE2, bcos_data$DSM8_FEV1_1, method = c("pearson"))$estimate
+correlation_age <- cor.test(bcos_data$FREG8_AGE, bcos_data$DSM8_FEV1_1, method = c("pearson"))$estimate
 
 pvalue_gender <- signif(dt(ttest_gender$statistic,df=(ttest_gender$parameter)),3)
 
@@ -36,13 +33,13 @@ B <- ggplot(bcos_data, mapping = aes(x = DBI14_WEIGHT , y = DSM8_FEV1_1)) +
   geom_smooth(method='lm', formula = y~x, color = "black") + 
   annotate("text", x =45, y = 5, label = paste("R=",(round(correlation_weight,3))))
 
-C <- ggplot(bcos_data, mapping = aes(x = FREG8_AGE2 , y = DSM8_FEV1_1)) + 
+C <- ggplot(bcos_data, mapping = aes(x = FREG8_AGE , y = DSM8_FEV1_1)) + 
   geom_point() +
   labs(x = "FREG8_AGE", y = "DSM8_FEV1_1", title = "FREG8_AGE & DSM8_FEV1_1") +
   geom_smooth(method='lm', formula = y~x, color = "black") + 
   annotate("text", x =70, y = 5, label = paste("R=",(round(correlation_age,3))))
 
-D <- ggplot(bcos_data, mapping = aes(x = as.factor(FREG7_GENDER2_LABEL) , y = DSM8_FEV1_1)) + 
+D <- ggplot(bcos_data, mapping = aes(x = as.factor(FREG7_GENDER_LABEL) , y = DSM8_FEV1_1)) + 
   geom_boxplot() +
   labs(x = "FREG7_GENDER", y = "DSM8_FEV1_1", title = "FREG7_GENDER & DSM8_FEV1_1") +
   annotate("text", x = 1.5, y = 5, label = paste("P value =",(pvalue_gender)))
